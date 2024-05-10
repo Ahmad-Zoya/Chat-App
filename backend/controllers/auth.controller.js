01490978dd7c43d0e43bb13de586f33f965baf2e
Ahmad-Zoya <118563741+Ahmad-Zoya@users.noreply.github.com>
2024-05-10 22:27:42 +0530

    Add files via upload

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,12 @@
+export const signupUser = (req, res) => {
+    console.log("signupUser");
+}
+
+export const loginUser = (req, res) => {
+    console.log("loginUser");
+}
+
+export const logoutUser = (req, res) => {
+    console.log("logoutUser");
+}
+
